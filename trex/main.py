ba2c95a0638ac0a18abc62c0a714c1bf3a3b6e04
anton krotov <twinkidr@gmail.com>
2026-05-16 01:16:06 +0800

    800 points

--- trex/main.py
+++ trex/main.py
@@ -2,13 +2,38 @@ import cv2
 import mss
 import pyautogui
 import numpy as np
+import time
 
 pyautogui.PAUSE = 0
 
-monitor = {"top": 285, "left": 520, "width": 30, "height": 35}
+JUMP_DURATION = 0.20
+DUCK_DURATION = 0.20
+
+MIN_JUMP = 0.05
+MIN_DUCK = 0.06
+
+start_time = time.time()
+last_speed_up_time = start_time
+
+cnt = 0
+
+monitor = {"top": 285, "left": 508, "width": 30, "height": 35}
 
 with mss.mss() as sct:
      while True:
+        current_time = time.time()
+
+        if current_time - last_speed_up_time >= 30.0:
+            if JUMP_DURATION > MIN_JUMP:
+                JUMP_DURATION = max(MIN_JUMP, JUMP_DURATION - 0.015)
+            if DUCK_DURATION > MIN_DUCK:
+                DUCK_DURATION = max(MIN_DUCK, DUCK_DURATION - 0.015)
+
+            last_speed_up_time = current_time
+            cnt += 1
+
+        if cnt % 2 == 0 and cnt > 0:
+            monitor['width'] += 5
 
         img = np.array(sct.grab(monitor))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,12 +47,17 @@ with mss.mss() as sct:
 
         if np.mean(bottom_half) < 247:
             pyautogui.press('space')
-            # pyautogui.press('down')
+            time.sleep(JUMP_DURATION)
+            pyautogui.keyDown('down')
+            time.sleep(0.05)
+            pyautogui.keyUp('down')
 
         elif np.mean(top_half) < 247:
             pyautogui.keyDown('down')
+            time.sleep(DUCK_DURATION)
+            pyautogui.keyUp('down')
 
-        cv2.imshow("Debug", gray)
+        cv2.imshow("game", gray)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
